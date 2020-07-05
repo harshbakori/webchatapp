f83f5ec35f35f0c3e9b0c4808093e12b5f88ca54
D3@D53C71 <30556333+harshbakori@users.noreply.github.com>
2020-07-05 21:55:07 +0530

    mongoose connected

--- server.js
+++ server.js
@@ -5,35 +5,46 @@ var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
+var mongoose = require('mongoose')
+
 app.use(express.static(__dirname))
 app.use(bodyparser.json())
-
 app.use(bodyparser.urlencoded({ extended: true }))
 
-var messages = [{
-    name: "tim",
-    messages: "hi"
-}, {
-    name: "jorden",
-    messages: "hello"
-}]
+var dburl = 'mongodb+srv://user:user@cluster0.p3usy.mongodb.net/Cluster0?retryWrites=true&w=majority'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    messages: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) => {
-    console.log(req.body)
-    messages.push(req.body)
-    io.emit('messages', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
 
+    message.save((err) => {
+        console.log("the error in mongoconnect = " + err)
+        if (err) { sendStatus(500) } else {
+            console.log(req.body)
+                // messages.push(req.body)
+            io.emit('messages', req.body)
+            res.sendStatus(200)
+        }
+    })
 })
 
 io.on('connection', (socket) => {
-    console.log("user connected")
+    console.log("user connected to io")
 })
 
+mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    console.log("mongodb connection", err)
+})
 
 var server = http.listen(3000, () => {
     console.log("server is listnning to the port :", server.address().port)
